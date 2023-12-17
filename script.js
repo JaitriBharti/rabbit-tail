f8cb202ca6234459900bac58167b74107388cf2a
JaitriBharti <144986997+JaitriBharti@users.noreply.github.com>
2023-12-18 00:16:56 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+var swiper = new Swiper(".homeslider", {
+    pagination: {
+      el: ".swiper-pagination",
+    },
+    autoplay: {
+        delay: 3000,
+        diaableOnInteraction: false
+    },
+    loop: true,
+  });
